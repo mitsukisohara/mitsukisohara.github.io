354d55804b160ee7688df6ee4240586a462789da
mitsukisohara <1023930132zkh@gmail.com>
2024-12-03 08:20:30 +0800

    DIP

--- 1.py
+++ 1.py
@@ -8,7 +8,7 @@ with open('static/demo/kpts_forground.json') as f:
 print(len(data_f))
 print(len(data_b[0]))
 print(len(data_b[0][0]))
-# for i in range(len(data_f)):
-#     data_f[i] = data_f[i] + data_b[i]
-# print(len(data_f[0]))
-# json.dump(data_f, open('static/demo/kpts_all.json', 'w'))
+for i in range(len(data_f)):
+    data_f[i] = data_f[i] + data_b[i]
+print(len(data_f[0]))
+json.dump(data_f, open('static/demo/kpts_new.json', 'w'))
